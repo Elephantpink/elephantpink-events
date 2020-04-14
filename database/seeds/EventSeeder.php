96baff5f7745b22aa5c7b84a809d096131316f94
dramirez <54021082+dramireztorres@users.noreply.github.com>
2020-04-14 14:28:20 +0200

    Update EventSeeder.php

--- database/seeds/EventSeeder.php
+++ database/seeds/EventSeeder.php
@@ -37,11 +37,7 @@ class EventSeeder extends Seeder
         Event::create([
             'name' => 'Meet & Greet',
             'location' => 'Málaga',
-            'description' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
-                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et 
-                quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
-                sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
-                voluptatem sequi nesciunt.',
+            'description' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
             'date' => $second_event_date
         ]);
 
@@ -56,10 +52,7 @@ class EventSeeder extends Seeder
         Event::create([
             'name' => 'Company Dinner',
             'location' => 'Marbella',
-            'description' => 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit 
-                laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit 
-                qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum 
-                fugiat quo voluptas nulla pariatur?',
+            'description' => 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
             'date' => $third_event_date
         ]);
 
@@ -73,10 +66,7 @@ class EventSeeder extends Seeder
         Event::create([
             'name' => 'Company Dinner',
             'location' => 'Marbella',
-            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui 
-                blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
-                excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia 
-                deserunt mollitia animi, id est laborum et dolorum fuga.',
+            'description' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
             'date' => $fourth_event_date
         ]);
     }
